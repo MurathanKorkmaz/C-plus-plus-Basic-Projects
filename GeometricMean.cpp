b5a5effdd881b278fee372bcfb60ca5e7e972217
MurathanKorkmaz <65683085+MurathanKorkmaz@users.noreply.github.com>
2020-05-25 13:51:34 +0300

    Add files via upload

---
+++ GeometricMean.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int counter=0;
+    double number=0;
+    double Product=1;
+    double Geometric=0;
+    cout<<"How many numbers will you enter : ";
+    cin>>counter;
+    int Vector[counter];
+    for(int i=0;i<counter;i++)
+    {
+        cout<<"Enter the "<<i+1<<"st issue : ";
+        cin>>number;
+        Vector[i]=number;
+    }
+    
+    for(int j=0;j<counter;j++)
+    {
+        Product=Product*Vector[j];
+    }
+    Geometric=pow(Product,1/(double)counter);
+    cout<<"Geometric average of the "<<counter<<" numbers entered : "<<Geometric<<endl;
+}
